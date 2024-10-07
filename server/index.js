8c38fcee45e17c2ddb1dc91b057b4fa5e24b17c5
Omkar <Omkaar1>
2024-10-07 16:17:36 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -6,9 +6,18 @@ const cors = require("cors");
 const router = require("./routes/routers");
 var bodyParser = require("body-parser");
 
+const corsOptions = {
+  origin: [
+    "http://localhost:5050",
+    "https://admin-dashboard-ochre-phi.vercel.app/",
+  ], // Add Vercel URL here
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 const port = 8080;
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
